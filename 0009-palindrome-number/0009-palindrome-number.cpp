e43f409e6e32f3657dc0b6f7bf70eb9854a57371
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-01 22:57:42 +0530

    Time: 4 ms (32.62%), Space: 8.6 MB (40.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long int sum=0,num=x;
+        while(num!=0)
+        {
+            sum=sum*10+num%10;
+            num/=10;
+        }
+        if (x<0) return false;
+        else if (sum==x) return true;
+        else return false;
+    }
+};
