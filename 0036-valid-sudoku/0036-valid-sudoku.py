c17bca7c8c4e13f2b5bfa5fcddf09054b1d37d45
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-01 23:39:35 +0530

    Time: 10 ms (35.64%), Space: 12.5 MB (18.44%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                ch = board[r][c]
+                if ch == '.':
+                    continue
+                b = (r // 3) * 3 + (c // 3)
+                if ch in rows[r] or ch in cols[c] or ch in boxes[b]:
+                    return False
+                rows[r].add(ch)
+                cols[c].add(ch)
+                boxes[b].add(ch)
+        return True
