b6283e15376bc9039fa2fe3d1b37824d025b212f
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-01 23:45:12 +0530

    Time: 0 ms (100%), Space: 17.8 MB (46.19%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        L,L1,N=[],[],numRows
+        for i in range(1,N+1):
+            a=1
+            L1=[]
+            for j in range(1,i+1):
+                L1.append(a)
+                a=int(a*(i-j)/j)
+            L.append(L1)
+        return L
