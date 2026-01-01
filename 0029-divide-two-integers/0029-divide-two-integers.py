d409f5d4293cb58f6e808584aa603188c334b7f3
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-01 23:39:12 +0530

    Time: 0 ms (100%), Space: 17.9 MB (18.54%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,14 +1,9 @@
-class Solution(object):
-    def divide(self, dividend, divisor):
-        """
-        :type dividend: int
-        :type divisor: int
-        :rtype: int
-        """
-        if dividend==0: return 0
-        elif divisor==1: return dividend
-        elif dividend==divisor: return 1
-        elif dividend<0 and divisor==-1: return -dividend-1
-        elif dividend<0 and divisor>0: return (-1)*((-dividend)/divisor)
-        elif dividend<0 and divisor<0 or dividend>0 and divisor>0: return dividend/divisor
-        elif dividend>0 and divisor<0: return -(dividend/-divisor)
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2**31 and divisor == -1: return 2**31 - 1
+        if dividend == 0: return 0
+        if divisor == 1: return dividend
+        if dividend == divisor: return 1
+        result=abs(dividend)//abs(divisor)
+        if (dividend<0 and divisor>=0) or (dividend>=0 and divisor<0): result=-result
+        return result
