71aebba7e94aed15d12099afe149074eefb43e93
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-01 22:59:12 +0530

    Time: 4 ms (19.24%), Space: 14.1 MB (12.05%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -0,0 +1,12 @@
+int maxArea(int* height, int heightSize) {
+    int max=0,i=0,j=heightSize-1,a;
+    while(i<j)
+    {
+        if (height[i]<height[j]) a=height[i]*(j-i);
+        else a=height[j]*(j-i);
+        if (a>max) max=a;
+        if (height[i]<height[j]) i=i+1;
+        else j=j-1;
+    }
+    return max;
+}
