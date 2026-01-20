ef99e34b18a17978dd9e80726d9e8fc5acaacc56
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-20 10:55:37 +0530

    Time: 5738 ms (25%), Space: 16.3 MB (25%) - LeetHub

---
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthAfterTransformations(self, s, t):
+        """
+        :type s: str
+        :type t: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        freq = [0] * 26
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+        for _ in range(t):
+            new_freq = [0] * 26
+            for i in range(25):  
+                new_freq[i + 1] = (new_freq[i + 1] + freq[i]) % MOD
+            new_freq[0] = (new_freq[0] + freq[25]) % MOD
+            new_freq[1] = (new_freq[1] + freq[25]) % MOD
+            freq = new_freq
+        return sum(freq) % MOD
