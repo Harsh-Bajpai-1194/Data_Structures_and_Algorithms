e038f422b34be830c29034d1a27d1e43fa5321db
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-20 10:57:09 +0530

    Time: 35 ms (89.39%), Space: 13.1 MB (12.12%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def doesAliceWin(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        for i in s:
+            if i in "aeiou": return True
+        return False
