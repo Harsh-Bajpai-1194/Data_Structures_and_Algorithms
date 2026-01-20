d28bc752b109e77a522c1f625ebb5475f0e00455
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-20 10:58:13 +0530

    Time: 0 ms (100%), Space: 12.4 MB (85.71%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isArraySpecial(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if (len(nums)==1): return True
+        if (nums[0]%2==nums[1]%2): return False
+        for i in range(len(nums)-1):
+            if (nums[i]%2==nums[i+1]%2): return False
+        return True
