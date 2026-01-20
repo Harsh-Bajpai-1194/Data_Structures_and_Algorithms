359fbbe7df7c83aced8a0c3d9ecac0c6d1b492c4
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-20 10:58:33 +0530

    Time: 0 ms (100%), Space: 27.6 MB (66.51%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        if (nums.size()==1) return true;
+        if (nums[0]%2==nums[1]%2) return false;
+        for (int i=0; i<nums.size()-1; i++)
+        {
+            if (nums[i]%2==nums[i+1]%2) return false;
+        }
+        return true;
+    }
+};
