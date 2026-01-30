3e683612adf4dd9ce79dcadf27b17bd7719cf0fd
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-01-30 13:56:25 +0530

    Time: 0 ms (100%), Space: 9.9 MB (19.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.c
+++ 0042-trapping-rain-water/0042-trapping-rain-water.c
@@ -1,23 +1,19 @@
 int trap(int* height, int heightSize) {
-    int n = heightSize;
-    if (n == 0) return 0;
-    int Prefix_Max[n],Suffix_Max[n];
-    Prefix_Max[0] = height[0];
-    for (int i = 1; i < n; i++) 
+    int ans=0,lmax=0,rmax=0,l=0,r=heightSize-1;
+    while (l<r)
     {
-        if (height[i] > Prefix_Max[i-1]) Prefix_Max[i] = height[i];
-        else Prefix_Max[i] = Prefix_Max[i-1];
-    }
-    Suffix_Max[n-1] = height[n-1];
-    for (int i = n - 2; i >= 0; i--) 
-    {
-        if (height[i] > Suffix_Max[i+1]) Suffix_Max[i] = height[i];
-        else Suffix_Max[i] = Suffix_Max[i+1];
-    }
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        int min_height = (Prefix_Max[i] < Suffix_Max[i]) ? Prefix_Max[i] : Suffix_Max[i];
-        ans += min_height - height[i];
+        if (height[l]<=height[r])
+        {
+            if (lmax>height[l]) ans+=lmax-height[l];
+            else lmax=height[l];
+            l++;
+        }
+        else
+        {
+            if (rmax>height[r]) ans+=rmax-height[r];
+            else rmax=height[r];
+            r--;
+        }
     }
     return ans;
 }
