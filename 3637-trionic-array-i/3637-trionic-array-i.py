8d77d5b8aa09483bac5cff65a3d54ad3b4bf244f
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-02-03 20:21:02 +0530

    Time: 1 ms (48.28%), Space: 12.4 MB (65.52%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isTrionic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        i = 1
+        while i < n and nums[i - 1] < nums[i]: i += 1
+        p = i - 1
+        while i < n and nums[i - 1] > nums[i]: i += 1
+        q = i - 1
+        while i < n and nums[i - 1] < nums[i]: i += 1
+        flag = i - 1
+        return (p != 0) and (q != p) and (flag == n - 1 and flag != q)
