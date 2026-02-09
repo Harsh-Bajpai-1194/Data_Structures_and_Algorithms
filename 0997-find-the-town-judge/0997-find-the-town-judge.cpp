9de20561feb6067dd47f1a1264139d3b9e0749de
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-02-09 19:16:48 +0530

    Time: 0 ms (100%), Space: 65.1 MB (83%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
+    int findJudge(int n, vector<vector<int>>& trust) 
+    {
+        vector<int> px(n+1), xp(n+1);
+        for(vector<int>& t:trust)
+        {
+            px[t[0]]++;
+            xp[t[1]]++;
+        }
         for (int i=1; i<=n; i++)
         {
-            int px=0, xp=0;
-            for(vector<int>& t:trust)
-            {
-                if (t[0]==i) 
-                {
-                    px++;
-                    break;
-                }
-                else if (t[1]==i) xp++;
-            }
-            if (px==0 && xp==n-1) return i;
+            if (px[i]==0 && xp[i]==n-1) return i;
         }
         return -1;
     }
