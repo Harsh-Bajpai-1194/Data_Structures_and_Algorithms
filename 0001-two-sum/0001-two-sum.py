ea11fbcd41cebdb690a4c666fe167f223af73ae9
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-03-09 22:53:23 +0530

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         L=[None,None]
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
+                if nums[i]+nums[j]==target: 
                     L[0]=i
                     L[1]=j
                     return L
