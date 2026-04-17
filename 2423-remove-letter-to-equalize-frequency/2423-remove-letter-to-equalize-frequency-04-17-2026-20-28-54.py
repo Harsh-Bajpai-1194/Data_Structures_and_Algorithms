aa9eec6d1349b325f27f2c7800ba9268b49e6c65
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-04-17 20:28:58 +0530

    N/A (0%) N/A (0%) 04-17-2026 2423-remove-letter-to-equalize-frequency Easy .py Hash Table

---
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency-04-17-2026-20-28-54.py
@@ -0,0 +1,17 @@
+class Solution:
+    def equalFrequency(self, word: str):
+        L,L1=[],[]
+        for i in word:
+            if [i,word.count(i)] not in L:
+                L.append([i,word.count(i)])
+                L1.append(word.count(i))
+        L1.sort()
+        if len(L1)==1 or len(set(L1))==1 and L1[0]==1:
+            return True
+        if len(L1)==2:
+            return (L1[0]==1 or L1[1]-L1[0]==1)
+        if L1[0]==1 and len(set(L1[1:]))==1:
+            return True
+        if L1[-1]-L1[-2]==1 and len(set(L1[:-1]))==1:
+            return True
+        return False
