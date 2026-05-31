714b45f487c27a35e494f5104ba5362d6f9d46a3
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-05-31 10:49:02 +0530

    Time: 31 ms (66.42%), Space: 106.9 MB (16.2%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(long int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        for (int i=0; i<asteroids.size(); i++)
+        {
+            if (mass<asteroids[i]) return false;
+            else mass+=asteroids[i];
+        }
+        return true;
+    }
+};
