b439e03a0c0171954246faf3d516eecc23ed90c7
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-09-01 19:17:59 +0530

    Time: 614 ms (7.19%), Space: 0B (100%) - LeetHub

---
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select email
+from (
+select email
+, count(id) as occurences
+from person
+group by email) as ABC
+where ABC.occurences > 1;
+/*
+select email
+from person
+group by email
+having count(id) > 1
+*/
