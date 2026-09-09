f008733b6c2225667de4c95ad2f7509dc34ecd0f
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-09-09 17:47:29 +0530

    Time: 0 ms (100%), Space: 19.1 MB (96.96%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -1,8 +1,4 @@
 class Solution:
-    def countCommas(self, n: int) -> int:
-        c=0; max=1000; d=1
-        while n>=max:
-            c+=(min((max*1000)-1,n)-max+1)*d
-            max*=1000
-            d+=1
-        return c
+    def countCommas(self, n: int) -> int:  
+        if n<999: return 0
+        return n-999
