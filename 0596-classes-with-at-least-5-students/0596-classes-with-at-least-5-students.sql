4b5b2844e4cd47482cebce1d961a580644ec9bf8
Harsh Bajpai <harshbajpai1194@gmail.com>
2026-09-11 19:03:21 +0530

    Time: 319 ms (89.51%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,4 +1,15 @@
 # Write your MySQL query statement below
+WITH students_per_class(class,students_count) AS
+(
+    SELECT class
+         , count(student) AS students_count
+    FROM Courses
+    GROUP BY class
+)
+SELECT class
+FROM students_per_class
+WHERE students_count >= 5;
+
 /*
 SELECT class
     , COUNT(student) as strength
@@ -6,7 +17,10 @@ SELECT class
     GROUP BY class) AS class_wise_strength
 WHERE strength >= 5;
 */
+
+/*
 SELECT class
     FROM Courses
     GROUP BY class
 HAVING COUNT(student) >= 5;
+*/
